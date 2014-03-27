b8a3b5a11b7ff8c411e026622f0fd98d39f231b0
Ben Dalton <bendalton@gmail.com>
2014-03-27 16:34:29 -0400

    Removing 'required' attribute on Schema structures

--- GeoJSON.js
+++ GeoJSON.js
@@ -1,62 +1,72 @@
 /**
- *  * GeoJSON Schemas for Mongoose
- *   *
- *    * rough GeoJSON schemas for use with mongoose schema creation
- *     *
- *      * Based on GeoJSON Spec @ http://geojson.org/geojson-spec.html
- *       *
- *        * Created by Ben Dalton (ben@rideamigos) on 3/27/14.
- *         * Copyright RideAmigos (http://rideamigos.com)
- *          */
+ * GeoJSON Schemas for Mongoose
+ * 
+ * rough GeoJSON schemas for use with mongoose schema creation
+ *   
+ * Based on GeoJSON Spec @ http://geojson.org/geojson-spec.html
+ *     
+ * Created by Ben Dalton (ben@rideamigos) on 3/27/14.
+ * Copyright RideAmigos (http://rideamigos.com)
+ **/
 
 var GeoJSON;
 module.exports = GeoJSON = {}
 
 GeoJSON.Geometry = {
-    'type': { type: String, required: true, enum:["Point", "MultiPoint", "LineString", "MultiLineString", "Polygon", "MultiPolygon"] },
-      coordinates:[{type:"Mixed"}]
+  'type'     : { type: String, enum: ["Point", "MultiPoint", "LineString", "MultiLineString", "Polygon", "MultiPolygon"] },
+  coordinates: []
 }
 
 GeoJSON.Point = {
-    'type': { type: String, required: true, default:"Point" },
-      coordinates:[{type:"Number"}]
+  'type'     : { type: String, default: "Point" },
+  coordinates: [
+    {type: "Number"}
+  ]
 }
 
 GeoJSON.MultiPoint = {
-    'type': { type: String, required: true, default:"MultiPoint" },
-      coordinates:[{type:"Array"}]
+  'type'     : { type: String, default: "MultiPoint" },
+  coordinates: [
+    {type: "Array"}
+  ]
 }
 
 GeoJSON.MultiLineString = {
-    'type': { type: String, required: true, default:"MultiLineString" },
-      coordinates:[{type:"Array"}]
+  'type'     : { type: String, default: "MultiLineString" },
+  coordinates: [
+    {type: "Array"}
+  ]
 }
 
 GeoJSON.Polygon = {
-    'type': { type: String, required: true, default:"Polygon" },
-      coordinates:[{type:"Array"}]
+  'type'     : { type: String, default: "Polygon" },
+  coordinates: [
+    {type: "Array"}
+  ]
 }
 
 GeoJSON.MultiPolygon = {
-    'type': { type: String, required: true, default:"MultiPolygon" },
-      coordinates:[{type:"Array"}]
+  'type'     : { type: String, default: "MultiPolygon" },
+  coordinates: [
+    {type: "Array"}
+  ]
 }
 
 GeoJSON.GeometryCollection = {
-    'type': { type: String, required: true, default:"GeometryCollection" },
-      geometries:[GeoJSON.Geometry]
+  'type'    : { type: String, default: "GeometryCollection" },
+  geometries: [GeoJSON.Geometry]
 }
 
 GeoJSON.Feature = {
-    id:{type:"String"},
-      'type': { type: String, required: true, default:"Feature" },
-        geometry:GeoJSON.Geometry,
-          properties:{type:"Object"}
+  id        : { type: "String" },
+  'type'    : { type: String, default: "Feature" },
+  geometry  : GeoJSON.Geometry,
+  properties: {type: "Object"}
 }
 
 GeoJSON.FeatureCollection = {
-    'type': { type: String, required: true, default:"FeatureCollection" },
-      features: [ GeoJSON.Feature ]
+  'type'  : { type: String, default: "FeatureCollection" },
+  features: [ GeoJSON.Feature ]
 }
 
 
