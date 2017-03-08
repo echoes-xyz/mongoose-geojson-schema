2f6caaaef2705deb6cef0715768f71003bcf8481
Josh Kopecek <josh@echoes.xyz>
2017-03-08 21:49:17 +0000

    Switch testing to Gulp Added gulp, fixing broken tests, also add .jshintrc to escape errors in integration files.

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp');
+const jshint = require('gulp-jshint');
+const mocha = require('gulp-mocha');
+const stylish = require('jshint-stylish');
+
+gulp.task('jshint', () => {
+    return gulp.src(['Gruntfile.js', 'GeoJSON.js', 'test/**/*.js'])
+        .pipe(jshint())
+        .pipe(jshint.reporter(stylish));
+});
+
+gulp.task('mocha', () => {
+    return gulp.src('test/**/*.integration.js', {read: false})
+    .pipe(mocha({reporter: 'spec'}));
+})
+
+gulp.task('default', ['jshint', 'mocha']);
