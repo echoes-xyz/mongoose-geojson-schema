e6f6e1b6f9403533932ca2a5d77d89939aabbb8d
Josh Kopecek <josh@echoes.xyz>
2023-01-10 09:08:15 +0000

    yarn remove unnecessary deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: ['airbnb-base'],
+  extends: ['eslint:recommended'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
